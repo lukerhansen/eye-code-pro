04ac93010e127817426235916ca860e677e64891
lukerhansen <67290335+lukerhansen@users.noreply.github.com>
2025-10-17 11:50:19 -0700

    stripe thing updated

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -9,7 +9,7 @@ import {
 import { SUBSCRIPTION_CONFIG } from '@/lib/config/subscription';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil'
+  apiVersion: '2025-08-27.basil'
 });
 
 export async function createCheckoutSession({
